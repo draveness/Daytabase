55ac077e162e9e0fbac496c7f618466b1d8e96bf
Draveness <i@draveness.me>
2017-03-01 14:28:49 +0800

    Add initialize method to Daytabase

--- Daytabase/Daytabase.swift
+++ Daytabase/Daytabase.swift
@@ -9,6 +9,28 @@
 import Foundation
 import sqlite3
 
+public typealias DaytabaseSerializer = (String, String, Any) -> Data
+public typealias DaytabaseDeserializer = (String, String, Data) -> Any?
+
+let defaultSerializer: DaytabaseSerializer = { (collection: String, key: String, value: Any) in
+    return NSKeyedArchiver.archivedData(withRootObject: value)
+}
+
+let defaultDeserializer: DaytabaseDeserializer = { (collection: String, key: String, data: Data) in
+    return NSKeyedUnarchiver.unarchiveObject(with: data)
+}
+
 public final class Daytabase {
+    public let databasePath: String
+    public let objectSerializer: DaytabaseSerializer
+    public let objectDeserializer: DaytabaseDeserializer
+    
+    init(path: String,
+         serializer: @escaping DaytabaseSerializer = defaultSerializer,
+         deserializer: @escaping DaytabaseDeserializer = defaultDeserializer) {
+        self.databasePath = path
+        self.objectSerializer = serializer
+        self.objectDeserializer = deserializer
+    }
     
 }
