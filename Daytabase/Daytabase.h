13a87b13ba0b1b0f632027017c37355e988062f4
Draveness <i@draveness.me>
2017-03-01 13:34:27 +0800

    Initial Commit

---
+++ Daytabase/Daytabase.h
@@ -0,0 +1,19 @@
+//
+//  Daytabase.h
+//  Daytabase
+//
+//  Created by Draveness on 01/03/2017.
+//  Copyright © 2017 Draveness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Daytabase.
+FOUNDATION_EXPORT double DaytabaseVersionNumber;
+
+//! Project version string for Daytabase.
+FOUNDATION_EXPORT const unsigned char DaytabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Daytabase/PublicHeader.h>
+
+
